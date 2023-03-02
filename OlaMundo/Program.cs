fcf2b242591edc9d7954ed278734e92dd020f8af
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-02 13:29:29 +0000

    Add /// before main and program

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
 namespace OlaMundo
+///
 {
     class Program
     {
+        ///
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
