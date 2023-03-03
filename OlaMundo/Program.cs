84627ab6bfd60be966ca7fdf9cbf1fd3f1075c95
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-03 12:23:03 +0000

    Add new descriptions

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 
 namespace OlaMundo
-///
+///Code to write Hello World and Bye World 
+///  
 {
     class Program
     {
-        ///
+        /// <summary>
+        /// Main function to write Hello World and Bye world on console
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
