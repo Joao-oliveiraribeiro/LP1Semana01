17bb4370f825294a696ae7c6baac3d91e6745071
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-03 11:18:36 +0000

    Add string variables

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string starting_message = "Welcome";
+            string gameover_message = "You lost!";
+            string message_and_copyright = "This message is subject to copyright\u00A9";
+            string message_with_verbatim = @"Would you like to continue? \n Y/N";
+            string message_with_unicode = "0x0027";
+            string unicode_with_escape = "u\03B2";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(starting_message);
+            Console.WriteLine(gameover_message);
+            Console.WriteLine(message_and_copyright);
+            Console.WriteLine(message_with_verbatim);
+            Console.WriteLine(message_with_unicode);
+            Console.WriteLine(unicode_with_escape);
+            
+        }
+    }
+}
