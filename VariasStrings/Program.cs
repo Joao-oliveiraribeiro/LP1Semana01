6adcbe59b7d39b163b98ab9b3585b5d53f970cb1
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-03 11:53:20 +0000

    Add new strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -12,6 +12,16 @@ namespace VariasStrings
             string message_with_verbatim = @"Would you like to continue? Y/N";
             string message_with_unicode = "Unicode exclamaçao \u0021";
             string unicode_with_escape = "Unicode letra em latim \u03B2";
+            string operador_mais = "Uma"+"string"+"adcionada"+"com +";
+            string x = "Hello";
+            int y= 10;
+            int z=5;
+            string name = "programador";
+            string adicionar_uma_variavel = x + "World";
+            string interpolaçao = $"O y equivale a {y}";
+            string soma = $"Para somar y={y} + z={z} fica {y + z}";
+            string hello_and_name =String.Format("Hello {0}, name");
+            string valores_y_z =String.Format("y e z sao {0} e {1} ",y,z);
             Console.WriteLine("Hello, World!");
             Console.WriteLine(starting_message);
             Console.WriteLine(gameover_message);
@@ -19,6 +29,13 @@ namespace VariasStrings
             Console.WriteLine(message_with_verbatim);
             Console.WriteLine(message_with_unicode);
             Console.WriteLine(unicode_with_escape);
+            Console.WriteLine(adicionar_uma_variavel);
+            Console.WriteLine(interpolaçao);
+            Console.WriteLine(soma);
+            Console.WriteLine(hello_and_name);
+            Console.WriteLine(valores_y_z);
+            Console.WriteLine("Valor de z e y são {0} e {1}",z,y);
+            Console.WriteLine(@"Uso de verbatim e unicode \u0021");
             
         }
     }
