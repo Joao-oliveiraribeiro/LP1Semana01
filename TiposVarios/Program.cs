707cf66d47c4c4ca15608ffcf32ea0d4c1d566eb
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-03 10:24:37 +0000

    Add integer variables

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        int value = 8;
+        decimal pi = 3.1415m;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine;
+        }
+    }
+}
