e8e9373e1b039d1ae1e02689d697f102ec35b611
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-19 22:09:49 +0000

    Add program description

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,6 +2,9 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Program to write predefined strings
+    /// </summary>
     public class Program
     {
         private static void Main(string[] args)
