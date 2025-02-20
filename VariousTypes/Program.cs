bf56b29aab182a2d576c7cd18d2b633b98461c8c
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-20 17:04:27 +0000

    Add char type

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -8,12 +8,14 @@ namespace VariousTypes
         {
             decimal pi = 3.1415m;
             int value = 8;
+            char copyright ='\u00A9';
+            char z_min = '\u007A';
 
-            
             Console.WriteLine("Hello LP1!");
             Console.WriteLine(value);
             Console.WriteLine(pi);
-
+            Console.WriteLine(copyright);
+            Console.WriteLine(z_min);
         }
     }
 }
