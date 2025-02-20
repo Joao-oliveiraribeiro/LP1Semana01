ef8a891d11e88e5be4150135f549589dcff55cc6
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-20 17:01:25 +0000

    Add decimal values

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,7 +6,14 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            decimal pi = 3.1415m;
+            int value = 8;
+
+            
             Console.WriteLine("Hello LP1!");
+            Console.WriteLine(value);
+            Console.WriteLine(pi);
+
         }
     }
 }
