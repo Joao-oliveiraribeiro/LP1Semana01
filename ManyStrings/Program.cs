e82e9c1f7c4ea6e92d7105b0323c05ef1fb6b692
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-20 17:36:25 +0000

    Add ex6

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,8 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
             string starting = "Hello";
             string close = "Bye";
             string copyright = "This is the copyright symbol \u00A9";
@@ -17,7 +19,8 @@ namespace ManyStrings
             string add_variable = y + "Hello";
             string interpolation = $"Y is equal to {y}";
             string add_sum = $"The additive of y ={y} + x ={x} is done like {y + x}";
-
+            string s1 = $"xx= {xx:f2} and {xx:p1}";
+            string s2 = $"ii = {ii:x} in hexadecimal and {ii:c} in euros";
             Console.WriteLine("Hello LP1!");
             Console.WriteLine(starting);
             Console.WriteLine(close);
@@ -27,6 +30,9 @@ namespace ManyStrings
             Console.WriteLine(add_variable);
             Console.WriteLine(interpolation);
             Console.WriteLine(add_sum);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            
         }
     }
 }
