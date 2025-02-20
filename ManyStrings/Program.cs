927776b5e8bc5d8881dadd4bfc1315f32c37c9fc
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-20 17:27:12 +0000

    Add new string types

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -12,6 +12,12 @@ namespace ManyStrings
             string unicode = "Unicode exclamacao \u0021";
             string name = "Luis";
             string hello_name =String.Format("Hello {0}",name);
+            int y = 5;
+            int x = 3;
+            string add_variable = y + "Hello";
+            string interpolation = $"Y is equal to {y}";
+            string add_sum = $"The additive of y ={y} + x ={x} is done like {y + x}";
+
             Console.WriteLine("Hello LP1!");
             Console.WriteLine(starting);
             Console.WriteLine(close);
