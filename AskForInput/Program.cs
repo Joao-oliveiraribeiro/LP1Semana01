a10b5879b33cb328b8097b351eacfcecb3f5af3a
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-21 09:12:46 +0000

    Ask user for int number

---
+++ AskForInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um numero inteiro: ");
+            string inputInt = Console.ReadLine();
+            
+        }
+    }
+}
